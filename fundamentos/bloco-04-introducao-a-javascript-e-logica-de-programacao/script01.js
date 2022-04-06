fae43df03c25e81f5aeaaddf08b35dcd2b4f2ce8
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-06 19:25:49 -0300

    add calculator exercise

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/script01.js
@@ -0,0 +1,10 @@
+const a = 2;
+const b = 3;
+
+console.log(`${a} + ${b} = ${a + b}`);
+console.log(`${a} - ${b} = ${a - b}`);
+console.log(`${a} x ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
+console.log(`${a} / ${b} tem como resto da divisão ${a % b}`); 
+
+//Foi usado como referência a documentação do mozilla: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
