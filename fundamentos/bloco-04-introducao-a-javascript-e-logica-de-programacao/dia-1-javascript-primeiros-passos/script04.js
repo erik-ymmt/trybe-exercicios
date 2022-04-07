71596a1b1e6ef13f945a42d7e307e2b9dc2ee884
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-06 22:20:11 -0300

    add script04 - positive/negative

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script04.js
@@ -0,0 +1,11 @@
+const a = 0;
+
+if (a > 0) {
+    console.log('positive');
+} else if (a < 0) {
+    console.log('negative');
+} else if (a == 0) {
+    console.log('zero');
+} else {
+    console.log('invalid value, must be a number')
+}
