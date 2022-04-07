9145dda4ae96dc87dd2c0ed35ebe5785f9be8516
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-06 23:12:13 -0300

    add script08 - uneven number

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script08.js
@@ -0,0 +1,19 @@
+const num1 = 13;
+const num2 = 4;
+const num3 = 8;
+
+//check if number is uneven
+function uneven (num) {
+    if(num % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//check if any of the 3 numbers is uneven
+if (uneven(num1) == true || uneven(num2) == true || uneven(num3) == true ) {
+    console.log('true. There is an uneven number.');
+} else {
+    console.log('false. They are all even.');
+}
