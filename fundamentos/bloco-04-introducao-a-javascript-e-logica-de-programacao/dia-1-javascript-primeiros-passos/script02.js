96234ff53dbfa4c82689519d9f031c2d51cea912
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-06 22:08:44 -0300

    add script02 - returns higher number

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script02.js
@@ -0,0 +1,11 @@
+const a = 20;
+const b = 20;
+const texto = ' é o maior número.'
+
+if (a > b) {
+    console.log(a + texto);
+} else if (b > a) {
+    console.log(b + texto);
+} else {
+    console.log('Valores iguais');
+}
