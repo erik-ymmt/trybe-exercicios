75af1431127a0ca5c76b10c84d4dbf6f4dce094c
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-18 19:07:47 -0300

    add parte II

--- fundamentos/bloco-05-js-dom-e-web-storage/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-05-js-dom-e-web-storage/dia-3-javascript-eventos/main.js
@@ -21,30 +21,30 @@ function addClassTech(elemento) {
     elemento.target.className = 'tech';
 }
 
-document.addEventListener('click', addClassTech);
+firstLi.addEventListener('click', addClassTech);
+secondLi.addEventListener('click', addClassTech);
+thirdLi.addEventListener('click', addClassTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+let inputDaCaixinha = 
+input.addEventListener('input', function (event) {
+    for (let i = 0; i < techElements.length; i += 1) {
+        techElements[0].innerText = event.target.value;
+    }
+});
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portfólio?
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página; 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function () {
+    window.location.replace('https://erik-ymmt.github.io/');
+});
 
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
+myWebpage.addEventListener('mouseenter', function (event) {
+    event.target.style.color = '#2fc18c';
+    console.log(event);
+});
 
-// Segue abaixo um exemplo do uso de event.target:
-
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
-
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+myWebpage.addEventListener('mouseout', function(event) {
+    event.target.style.color = 'white';
+  });
