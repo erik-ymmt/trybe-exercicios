f2fb2e52fa6f8bd3fb3e426fc062d5c3d581ddd6
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-26 20:52:36 -0300

    add bonus requirements

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/1-exercícios/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/1-exercícios/script.js
@@ -1,5 +1,33 @@
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', function (event) {
+
   event.preventDefault();
+
+  const nameInput = document.getElementById('input-name').value;
+  const emailInput = document.getElementById('input-email').value;
+  const textInput = document.getElementById('input-textarea').value;
+  const imgRightsCheckbox = document.getElementById('term2');
+  let errorVerificator = false;
+
+  if (nameInput.length < 10 || nameInput > 40) {
+    alert('Nome inválido, deve conter de 10 a 40 caracteres');
+    errorVerificator = true;
+  }
+  if (emailInput.length < 10 || emailInput > 50) {
+    alert('Email inválido, deve conter de 10 a 50 caracteres');
+    errorVerificator = true;
+  }
+  if (textInput.length > 500) {
+    alert(`Resposta inválida, deve conter até 500 caracteres. Número de caracteres atual: ${textInput.length}`);
+    errorVerificator = true;
+  }
+
+  if (imgRightsCheckbox.checked === false) {
+    alert('Por favor, aceite os termos de imagem.')
+  }  
+
+  if (errorVerificator === false && imgRightsCheckbox.checked) {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
+  }  
 });
