9f3b64750751a4c7de7aca9a1e3152ecd5b7e0f1
Erik Yamamoto <erik.ymmt@gmail.com>
2022-04-28 20:17:57 -0300

    add bonus requirements

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,42 +1,85 @@
-const submitButton = document.getElementById('submit-button');
+const submitButton = document.getElementById('submit-btn');
+const textInput = document.getElementById('input-textarea');
+const numberOfChars = document.getElementById('number-chars');
 
-submitButton.addEventListener('click', function (event) {
+window.addEventListener('DOMContentLoaded', datePicker);
+textInput.addEventListener('keyup', textInputCounter);
 
-  event.preventDefault();
-
-  const nameInput = document.getElementById('input-name').value;
-  const emailInput = document.getElementById('input-email').value;
-  const textInput = document.getElementById('input-textarea').value;
-  const imgRightsCheckbox = document.getElementById('term2');
-  let errorVerificator = false;
-
-function validateCharLength(input, min, max, inputLabel) {
-  if (input.length < min || input.length > max) {
-    alert(`${inputLabel} inválido, deve conter de ${min} a ${max} caracteres`);
-    errorVerificator = true;
-  }
+function textInputCounter() {
+  let charsCounter = `caracteres: ${textInput.value.length}`;
+  numberOfChars.innerText = charsCounter;;
 }
 
-validateCharLength(nameInput, 10, 40, 'Nome');
-validateCharLength(emailInput, 10, 50, 'Email');
-validateCharLength(textInput, 0, 500, 'Resposta');
-
-  if (imgRightsCheckbox.checked === false) {
-    alert('Por favor, aceite os termos de imagem.')
-  }  
-
-  if (errorVerificator === false && imgRightsCheckbox.checked) {
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.')
-  }  
-});
-
-window.addEventListener('DOMContentLoaded', function() {
+function datePicker() {
   const datePicker = document.getElementById('datepicker');
 
   datePicker.DatePickerX.init({
     format: 'dd/mm/yyyy'
   });
       clearButton: false
+};
+// Referência: https://github.com/AlexKrupko/DatePickerX
+
+const validate = new JustValidate('#form');
+
+validate
+  .addField('#input-name', [
+    {
+      rule: 'required',
+      errorMessage: 'O campo de nome é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+      errorMessage: 'Por favor, digite no máximo 40 caracteres.',
+    },
+    {
+      rule: 'minLength',
+      value: 10,
+      errorMessage: 'Por favor, digite no mínimo 10 caracteres.',
+    },
+  ])
+  .addField('#input-email', [
+    {
+      rule: 'required',
+      errorMessage: 'Campo obrigatório.',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'O email é inválido.',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+      errorMessage: 'Por favor, digite no máximo 50 caracteres.',
+    },
+  ])
+  .addField('#input-textarea', [
+    {
+      rule: 'required',
+      errorMessage: 'O campo de resposta é obrigatório.',
+    },
+    {
+      rule: 'maxLength',
+      value: 500,
+      errorMessage: `Por favor, digite no máximo 500 caracteres.`,
+    },
+  ])
+  .addField('#datepicker', [
+    {
+      rule: 'required',
+      errorMessage: 'O campo de data é obrigatório.',
+    },
+  ])
+  .addField('#term2', [
+    {
+      rule: 'required',
+      errorMessage: 'Por favor, concorde com a política de imagens',
+    },
+  ])
+  .onSuccess(function () {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
   });
 
-  // Referência: https://github.com/AlexKrupko/DatePickerX
+// Referência: https://github.com/horprogs/Just-validate
+  
