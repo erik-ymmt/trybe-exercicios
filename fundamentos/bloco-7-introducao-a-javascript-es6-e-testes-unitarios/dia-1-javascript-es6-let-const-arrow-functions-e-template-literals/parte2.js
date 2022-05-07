dd3569dea85b6fd8c812ec235c5799eafda5930f
Erik Yamamoto <erik.ymmt@gmail.com>
2022-05-07 20:25:59 -0300

    add exercise 2 - parte2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2.js
@@ -1,6 +1,9 @@
 // Crie uma função que receba um número e retorne seu fatorial.
 const randomNumber = Math.floor(Math.random() * 10);
 const factorial = (number) => {
+  if (number === 0) {
+    return 1;
+  }
   let result = number;
   for (let index = number - 1; index > 1; index -= 1) {
     result *= index;    
@@ -9,3 +12,18 @@ const factorial = (number) => {
 }
 console.log(randomNumber);
 console.log(factorial(randomNumber));
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+const exemple = 'Antônio foi no banheiro e não sabemos o que aconteceu, paralelepipedo!'
+const longestWordInPhrase = (phrase) => {
+  const splitPhrase = phrase.split(' ');
+  let longestWord = splitPhrase[0];
+  for (index in splitPhrase) {
+    if (splitPhrase[index].length > splitPhrase[0].length) {
+      longestWord = splitPhrase[index]
+    }
+  }
+  return longestWord;
+}
+
+console.log(longestWordInPhrase(exemple));
