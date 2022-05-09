8b069a4ba81bb8e4da3bd5650e93618e7644c39c
Erik Yamamoto <erik.ymmt@gmail.com>
2022-05-09 15:43:57 -0300

    add class exercise - parte II

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-objects/0-aula.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-objects/0-aula.js
@@ -12,3 +12,37 @@ const user1 = {
 console.log(user1);
 addNewKey(user1, 'age', 27);
 console.log(user1);
+
+var an_obj = { 100: 'a', 2: 'b', 7: 'c' };
+for (keys in an_obj) {
+  console.log(keys);
+}
+console.log(Object.keys(an_obj));
+
+// criar uma função que exiba as habilidades do objeto student. Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade"
+
+const getStudentsSkills = (studentObject) => {
+  const objectKeys = Object.keys(studentObject);
+  for (key of objectKeys) {
+    console.log(`${key}, Nível: ${studentObject[key]}`);
+  }
+}
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+getStudentsSkills(student1);
+getStudentsSkills(student2);
+
