ba61620d88bc4bb0686b289a2c5cb88c6433893d
Erik Yamamoto <erik.ymmt@gmail.com>
2022-05-13 21:58:58 -0300

    add exercise bonus - parte II + start game

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -1,13 +1,15 @@
 // Parte I - Game Actions Simulator
 
 const mage = {
+  name: 'Mago',
   healthPoints: 130,
   intelligence: 45,
-  mana: 125,
+  mana: 75,
   damage: undefined,
 };
 
 const warrior = {
+  name: 'Guerreiro',
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
@@ -15,8 +17,9 @@ const warrior = {
 };
 
 const dragon = {
+  name: 'Dragão',
   healthPoints: 350,
-  strength: 50,
+  strength: 100,
   damage: undefined,
 };
 
@@ -41,11 +44,77 @@ const warriorDamage = () => {
 // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
 
 const mageDmgAndManaUsage = () => {
-  let mageDmg = Math.floor(Math.random() * ((mage['intelligence'] * 2) + 1 - mage['intelligence'] )) + mage['intelligence'];
+  let mageDamage = Math.floor(Math.random() * ((mage['intelligence'] * 2) + 1 - mage['intelligence'] )) + mage['intelligence'];
   let manaUsage = 15;
   if (mage['mana'] < 15) {
-    mageDmg = 'Não possui mana suficiente';
+    mageDamage = 0; // Sem mana;
     manaUsage = 0;
   }
-  return { mageDmg, manaUsage }
+  return { 
+    'mageDamage': mageDamage,
+    'manaUsage': manaUsage }
 }
+
+// Parte II
+// 1 - Crie a primeira HOF que compõe o objeto gameActions. Ela será a função que simula o turno do personagem warrior. Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon. Além disto ela também deve atualizar o valor da chave damage do warrior.
+
+const warriorAttacks = (warriorDmg) => {
+  warrior['damage'] = warriorDmg();
+  dragon['healthPoints'] -= warrior['damage'];
+}
+
+// 2 - Crie a segunda HOF que compõe o objeto gameActions. Ela será a função que simula o turno do personagem mage. Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará os healthPoints do monstro dragon. Além disto ela também deve atualizar o valor das chaves damage e mana do mage.
+
+const mageAttacks = (mageDmg) => {
+  mage['mana'] -= mageDmg()['manaUsage'];
+  mage['damage'] = mageDmg()['mageDamage'];
+  dragon['healthPoints'] -= mage['damage'];
+}
+
+// 3 - Crie a terceira HOF que compõe o objeto gameActions. Ela será a função que simula o turno do monstro dragon. Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo monstro dragon e atualizará os healthPoints dos personagens mage e warrior. Além disto ela também deve atualizar o valor da chave damage do monstro.
+
+const dragonAttacks = (dragonDmg) => {
+  dragon['damage'] = dragonDmg();
+  warrior['healthPoints'] -= dragon['damage'];
+  mage['healthPoints'] -= dragon['damage'];
+}
+
+// 4 - Adicione ao objeto gameActions uma função que retorne o objeto battleMembers atualizado e faça um console.log para visualizar o resultado final do turno.
+
+const turnResult = () => {
+  console.log(battleMembers);
+}
+
+const gameActions = {
+  warriorTurn: warriorAttacks,
+  mageTurn: mageAttacks,
+  dragonTurn: dragonAttacks,
+  turnResult: turnResult,
+  startGame: () => {
+    while ((warrior['healthPoints'] > 0 || mage['healthPoints'] > 0) && dragon['healthPoints'] > 0) {
+      gameActions.turnResult();
+      if (warrior['healthPoints'] > 0) {
+        gameActions.warriorTurn(warriorDamage)
+      };
+      if (mage['healthPoints'] > 0) {
+      gameActions.mageTurn(mageDmgAndManaUsage);
+      }
+      if (dragon['healthPoints'] > 0) {
+      gameActions.dragonTurn(dragonDamage);
+      }
+    }
+    if (warrior['healthPoints'] <= 0) {
+      console.log(`${warrior['name']} derrotado.`);
+    }
+    if (mage['healthPoints'] <= 0) {
+      console.log(`${mage['name']} derrotado.`);
+    }
+    if (dragon['healthPoints'] <= 0) {
+      console.log(`${dragon['name']} derrotado.`);
+    }
+  }
+};
+
+gameActions.startGame();
+
+// TODO: refatorar de maneira mais simples os ifs.
