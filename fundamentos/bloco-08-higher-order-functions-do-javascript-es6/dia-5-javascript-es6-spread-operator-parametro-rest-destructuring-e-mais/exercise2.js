0a382315140aaee11ae5ff0421ba70ecbad90f33
Erik Yamamoto <erik.ymmt@gmail.com>
2022-05-19 22:02:41 -0300

    add exercise 5

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise2.js
@@ -1,7 +1,2 @@
-// const sum = (...number) => 
-
-
-// console.log((1, 2, 1).forEach((element) => element++));
-
 const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
 console.log(sum(1, 1, 1, 1));
