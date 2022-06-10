da7deaa274b56fa5a586f5c38df7c0716c836654
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-10 15:55:46 -0300

    add exercise 2 and bonus

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/second-exercise/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <h1 class="footer" key="footer">E isso é só o começo...</h1>
+    );
+  }
+}
+
+export default Footer;
