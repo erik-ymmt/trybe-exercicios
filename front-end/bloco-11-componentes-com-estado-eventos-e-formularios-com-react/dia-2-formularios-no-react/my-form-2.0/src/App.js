c81ec116f923e5dc8bab5bcb89d10f7608f41eb6
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-29 18:52:41 -0300

    add day 11.2

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,66 +1,14 @@
 import React from 'react';
 import './App.css';
+import PersonalData from './components/PersonalData';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      name: '',
-      email: '',
-      cpf: '',
-      address: '',
-      city: '',
-      state: '',
-    }
-  }
-
-  handleInput( {target} ) {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    })
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Formulário</h1>
         <form className='form-control'>
-          <fieldset className='personal-infos'>
-            <label className='form-label'>
-              Nome
-              <input name="name" type="text" maxLength="40" style={{textTransform: 'uppercase'}} required className='form-control' />
-            </label>
-            <label name="email" className='form-label'>
-              Email
-              <input type="email" maxLength="50" required className='form-control' />
-            </label>
-            <label name="cpf" className='form-label'>
-              CPF
-              <input type="text" maxLength="11" required className='form-control' />
-            </label>
-            <label name ="address" className='form-label'>
-              Endereço
-              <input type="text" maxLength="200" required className='form-control' />
-            </label>
-            <label name="city" className='form-label'>
-              Cidade
-              <input type="text" maxLength="28" required onBlur className='form-control' />
-            </label>
-            <label name="state" className='form-label'>
-              Estado
-              <input type="comboBox" maxLength="28" required className='form-control' />
-            </label>
-            <label htmlFor='casa' className='form-label'>
-              Casa
-              <input htmlFor='casa' type="radio" name='tipo' />
-            </label>
-            <label htmlFor='apto' className='form-label'>
-              Apartamento
-              <input htmlFor='apto' type="radio" name='tipo' />
-            </label>
-          </fieldset>
+          <PersonalData />
           <fieldset className='last-job'>
           <label>
             Resumo do Currículo
@@ -79,6 +27,8 @@ class App extends React.Component {
             Salvar
           </button>
         </form>
+        <div className='data-collected'>
+        </div>
       </div>
     );
   }
