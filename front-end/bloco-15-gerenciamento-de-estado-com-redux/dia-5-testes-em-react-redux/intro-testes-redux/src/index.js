2f31b71deb4dcd6be0ebbb36f48b3d2852488b6e
Erik Yamamoto <erik.ymmt@gmail.com>
2022-07-27 14:35:37 -0300

    starting application

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/intro-testes-redux/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+
+// Store que foi provida pela nossa aplicação. Nos testes, precisamos prover uma nova store para podermos controlá-la
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
