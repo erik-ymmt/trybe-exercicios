7d07e43fdea7d4290c4610f41d77d49e586dc4e4
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-28 11:23:51 -0300

    add delete Simpson function

--- back-end/secao-4-nodejs/dia-1-intro-node/simpsons/index.js
+++ back-end/secao-4-nodejs/dia-1-intro-node/simpsons/index.js
@@ -1,40 +1,57 @@
 const {
-  promises: { readFile },
+  promises: { readFile, writeFile },
 } = require("fs");
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
-async function printSimpsons() {
+async function readSimpsons(file) {
   try {
-    const data = await readFile('./simpsons.json', 'utf-8');
+    const data = await readFile(file, "utf-8");
     const simpsonsName = JSON.parse(data);
-    simpsonsName.forEach((element) => {
-      console.log(`${element.id} - ${element.name}`);
-    });
+    return simpsonsName;
   } catch (error) {
     console.log(error);
   }
 }
 
+async function printSimpsons() {
+  const simpsonsName = await readSimpsons("./simpsons.json");
+  simpsonsName.forEach((element) => {
+    console.log(`${element.id} - ${element.name}`);
+  });
+}
+
 async function findSimpson(id) {
+  const simpsonsName = await readSimpsons("./simpsons.json");
+  const character = simpsonsName.find(
+    (character) => character.id === String(id)
+  );
+  if (character) {
+    console.log(`personagem: ${character.name}`);
+  } else {
+    console.log("id não encontrado");
+  }
+}
+
+async function deleteSimpson() {
+  const simpsonsNameData = await readSimpsons("./simpsons.json");
+  const newSimpsons = simpsonsNameData.filter(
+    (simpsonName) => simpsonName.id !== "10" && simpsonName.id !== "6"
+  );
+  deleteSimpson = JSON.stringify(newSimpsons)
   try {
-    const data = await readFile('./simpsons.json', 'utf-8');
-    const simpsonsName = JSON.parse(data);
-    const character = simpsonsName.find((character) => character.id === String(id));
-    if (character) { 
-      console.log(`personagem: ${character.name}`, )
-    } else {
-      console.log('id não encontrado');
-    };
-  } catch (error) {
-    console.log(error);
+    await writeFile("./simpsonsCopy.json", deleteSimpson);
+    console.log("sucesso!");
+  } catch (err) {
+    console.log("erro:", err);
   }
 }
 
 async function main() {
-  await printSimpsons();
-  const id = readline.questionInt('ID do personagem: ');
-  console.log('id:', id);
-  findSimpson(id);
+  // await printSimpsons();
+  // const id = readline.questionInt("ID do personagem: ");
+  // console.log("id:", id);
+  // findSimpson(id);
+  deleteSimpson();
 }
 
 main();
