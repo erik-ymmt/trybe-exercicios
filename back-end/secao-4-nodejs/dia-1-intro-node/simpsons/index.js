06857092c6739ebf02c88672fe181aeae2cd7d34
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-28 10:24:09 -0300

    add findSimpsons function

--- back-end/secao-4-nodejs/dia-1-intro-node/simpsons/index.js
+++ back-end/secao-4-nodejs/dia-1-intro-node/simpsons/index.js
@@ -1,8 +1,7 @@
 const {
   promises: { readFile },
 } = require("fs");
-
-// const simpsons = require("./simpsons");
+const readline = require('readline-sync');
 
 async function printSimpsons() {
   try {
@@ -16,4 +15,26 @@ async function printSimpsons() {
   }
 }
 
-printSimpsons();
+async function findSimpson(id) {
+  try {
+    const data = await readFile('./simpsons.json', 'utf-8');
+    const simpsonsName = JSON.parse(data);
+    const character = simpsonsName.find((character) => character.id === String(id));
+    if (character) { 
+      console.log(`personagem: ${character.name}`, )
+    } else {
+      console.log('id não encontrado');
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function main() {
+  await printSimpsons();
+  const id = readline.questionInt('ID do personagem: ');
+  console.log('id:', id);
+  findSimpson(id);
+}
+
+main();
