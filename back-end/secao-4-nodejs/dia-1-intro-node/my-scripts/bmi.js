09e66d7dff556d2b1395dd0ac0dccf105ba186b5
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-27 22:27:30 -0300

    feat: add situation levels

--- back-end/secao-4-nodejs/dia-1-intro-node/my-scripts/bmi.js
+++ back-end/secao-4-nodejs/dia-1-intro-node/my-scripts/bmi.js
@@ -1,11 +1,22 @@
 const readline = require('readline-sync');
 
-const height = readline.questionInt('What is your height in centimeters? ');
-const weight = readline.questionInt('What is your weight in kilograms? ');
+const height = readline.questionFloat('What is your height in centimeters? ');
+const weight = readline.questionFloat('What is your weight in kilograms? ');
 
 function calculateBmi(weight, height) {
   bmi = weight / ((height/100) ** 2);
-  return `${bmi}`;
+
+  if (bmi < 18.5) {
+    return `resultado: ${bmi}. Abaixo do peso (magreza).`
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    return `resultado: ${bmi}. Peso normal!`
+  } else if (bmi > 25.0 && bmi < 29.9) {
+    return `resultado: ${bmi}. Acima do peso (sobrepeso)`
+  } else if (bmi > 30 && bmi < 34.9) {
+    return `resultado: ${bmi}. Obesidade grau I`
+  } else {
+  return `resultado: ${bmi}. Obesidade grau II, II e IV`
+  }
 }
 
 console.log(calculateBmi(weight, height));
