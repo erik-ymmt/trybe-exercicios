57630c01516054f5516b68c992f27f7d361d7907
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-28 22:01:59 -0300

    exercise 4 - create GET activity by id - ok

--- back-end/secao-4-nodejs/dia-2-api-rest-express/to-do-list-exercise/src/app.js
+++ back-end/secao-4-nodejs/dia-2-api-rest-express/to-do-list-exercise/src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const activities = require('./data/activities');
 
 const app = express();
 
+app.get('/', (_req, res) => res.status(200).send(activities));
+
+app.get('/myActivities/:id', (req, res) => {
+  const activityId = req.params.id;
+
+  const selectedActivity = activities.find((activity) => activity.id === Number(activityId));
+
+  res.status(200).send(selectedActivity);
+});
 
 
 module.exports = app;
