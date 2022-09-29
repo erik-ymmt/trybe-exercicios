1aa2647eacc9449c90c78d487d539507a2343b73
Erik Yamamoto <erik.ymmt@gmail.com>
2022-09-28 22:24:19 -0300

    all exercises from to do list - ok

--- back-end/secao-4-nodejs/dia-2-api-rest-express/to-do-list-exercise/src/app.js
+++ back-end/secao-4-nodejs/dia-2-api-rest-express/to-do-list-exercise/src/app.js
@@ -3,15 +3,18 @@ const activities = require('./data/activities');
 
 const app = express();
 
-app.get('/', (_req, res) => res.status(200).send(activities));
+app.get('/myActivities', (_req, res) => res.status(200).json(activities));
 
 app.get('/myActivities/:id', (req, res) => {
   const activityId = req.params.id;
-
   const selectedActivity = activities.find((activity) => activity.id === Number(activityId));
-
-  res.status(200).send(selectedActivity);
+  res.status(200).json(selectedActivity);
 });
 
+app.get('/filter/myActivities', (req, res) => {
+  const activityStatus = req.query.status;
+  const filteredActivities = activities.filter((activity) => activity.status === activityStatus);
+  res.status(200).json(filteredActivities);
+});
 
 module.exports = app;
