e7c112d09dc70363ae1e417d395876330bcbe0aa
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-01 19:10:41 -0300

    add post method to add an activity

--- back-end/secao-4-nodejs/dia-4-middlewares/exercise-turismo/src/app.js
+++ back-end/secao-4-nodejs/dia-4-middlewares/exercise-turismo/src/app.js
@@ -1,7 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+const activities = [
+  {
+    "name": "Trekking",
+    "price": 0,
+    "description": {
+      "rating": 5,
+      "difficulty": "Fácil",
+      "createdAt": "10/08/2022"
+    }
+  }
+];
+
+// function validateActivity(req, res, next) {
+//   const newActivity = req.body;
+//   const requiredFields = ["name","price", "description"];
+//   const descriptionRequired = ["rating", "difficulty", "createdAt"];
+
+//   if() {
+//     return res.status(401).json({"message": "invalid format"})
+//   }
+//   next();
+// }
+
+app.post("/activities", validateActivity, (req, res) => {
+  const newActivity = req.body;
+  activities.push(newActivity);
+
+  res.status(201).json({ "message": "activity submitted with success!" });
+});
 
 module.exports = app;
