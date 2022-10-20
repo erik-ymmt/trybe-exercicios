0eeec8e75231b58f4564efca924bd5680d1e24dc
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-20 15:41:56 -0300

    class exercises done

--- back-end/secao-6-orm-e-autenticacao/dia-2-orm-associations/class-exercise/src/seeders/20221019185854-employees.js
+++ back-end/secao-6-orm-e-autenticacao/dia-2-orm-associations/class-exercise/src/seeders/20221019185854-employees.js
@@ -1,25 +1,18 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('employees',
+      [
+        { first_name: 'Marcos', last_name: 'Zuck', age: 49 },
+        { first_name: 'Fred', last_name: 'Mercurio', age: 19 },
+        { first_name: 'Ayrton', last_name: 'Keno', age: 51 },
+        { first_name: 'Robin', last_name: 'Mathias', age: 63 },
+        { first_name: 'Antonio', last_name: 'Augusto', age: 18 },
+      ],
+      {},
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkDelete('employees', null, {});
+  },
 };
--- back-end/secao-6-orm-e-autenticacao/dia-2-orm-associations/class-exercise/src/seeders/20221019185905-addresses.js
+++ back-end/secao-6-orm-e-autenticacao/dia-2-orm-associations/class-exercise/src/seeders/20221019185905-addresses.js
@@ -1,25 +1,50 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert(
+      "addresses",
+      [
+        {
+          city: "Belo Horizonte",
+          street: "Rua Florida",
+          number: 1080,
+          employee_id: 1,
+        },
+        {
+          city: "São Paulo",
+          street: "Avenida Paulista",
+          number: 1980,
+          employee_id: 2,
+        },
+        {
+          city: "Fortaleza",
+          street: "Rua das Enseadas",
+          number: 95,
+          employee_id: 3,
+        },
+        {
+          city: "Belo Horizonte",
+          street: "Rua Andaluzita",
+          number: 131,
+          employee_id: 4,
+        },
+        {
+          city: "Belo Horizonte",
+          street: "Rua Vicente Alvarenga",
+          number: 80,
+          employee_id: 1,
+        },
+        {
+          city: "Curitiba",
+          street: "Rua Fria",
+          number: 101,
+          employee_id: 5,
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkDelete("addresses", null, {});
+  },
 };
