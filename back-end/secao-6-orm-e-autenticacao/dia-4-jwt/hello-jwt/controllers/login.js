5854f8e5481e82f9467ca239ec016c7cb0e0f8c9
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-25 11:37:49 -0300

    exercise 2 - ok

--- back-end/secao-6-orm-e-autenticacao/dia-4-jwt/hello-jwt/controllers/login.js
+++ back-end/secao-6-orm-e-autenticacao/dia-4-jwt/hello-jwt/controllers/login.js
@@ -3,22 +3,36 @@ const jwt = require('jsonwebtoken');
 
 const secret = process.env.JWT_SECRET;
 
-module.exports = (req, res) => {
+const generateToken = (user) => {
+  const { username, password } = user;
+  let admin = false;
+
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  if (username === 'admin' && password === 's3nh4S3gur4???') {
+    admin = true;
+  }
+
+  const token = jwt.sign(
+    { data: { user: username, admin } },
+    secret,
+    jwtConfig,
+  );
+
+  return token;
+};
+
+const authenticateLogin = (req, res) => {
   const user = req.body;
   try {
-    const jwtConfig = {
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    };
-
-    const token = jwt.sign(
-      { data: { user: user.username, admin: false } },
-      secret,
-      jwtConfig,
-    );
-
+    const token = generateToken(user);
     res.status(200).json({ token });
   } catch (error) {
-    return res.status(500).json({ message: 'Erro interno', error: error.message });
+    res.status(500).json({ message: 'Erro interno', error: error.message });
   }
 };
+
+module.exports = authenticateLogin;
