335ef9b054d857d81adb99868484a94664b579d3
Erik Yamamoto <erik.ymmt@gmail.com>
2022-10-24 22:39:54 -0300

    add jwt config

--- back-end/secao-6-orm-e-autenticacao/dia-4-jwt/hello-jwt/controllers/login.js
+++ back-end/secao-6-orm-e-autenticacao/dia-4-jwt/hello-jwt/controllers/login.js
@@ -1,4 +1,24 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
 module.exports = (req, res) => {
   const user = req.body;
-  res.status(200).json({ message: `user ${user.username} logging in` });
+  try {
+    const jwtConfig = {
+      expiresIn: '7d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign(
+      { data: { user: user.username } },
+      secret,
+      jwtConfig,
+    );
+
+    res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno', error: error.message });
+  }
 };
