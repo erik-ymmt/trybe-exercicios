0b9d2e391c96748b9c128e9009cdad6850599531
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-09 16:10:55 -0300

    add exercise 2

--- back-end/secao-9-POO-SOLID/dia-1-intro-POO/class-exercises/paraFixar.ts
+++ back-end/secao-9-POO-SOLID/dia-1-intro-POO/class-exercises/paraFixar.ts
@@ -1,3 +1,4 @@
+// exercise 1
 class Tv {
   brand: string;
   size: number;
@@ -20,3 +21,57 @@ class Tv {
 const tvSamsung = new Tv('samsumg', 32, '4k', ['hdmi']);
 
 tvSamsung.turnOn();
+
+// exercise 2
+class Person {
+  name: string;
+  private _weight: number;
+  private _age: number;
+  readonly height: number;
+
+  constructor(name: string, height: number, weight: number, age: number) {
+    this.name = name;
+    this._weight = weight;
+    this._age = age;
+    this.height = height;
+  }
+
+  getWeight() {
+    return this._weight;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newValue: number) {
+    if (newValue >= 0 && newValue < 200) {
+      this._age = newValue;
+    }
+  }
+
+  birthday() {
+    this._age += 1;
+  }
+
+}
+
+const p1 = new Person('Maria', 171, 58, 19);
+const p2 = new Person('João', 175, 66, 18);
+
+p1.name = 'Erik';
+console.log(p1.name);
+console.log(p1.getWeight());
+console.log(p2.age);
+p2.birthday();
+console.log(p2.age);
+p1.age = 28;
+console.log(p1.age);
+console.log(p1.height);
+
+
+
+
+
+
+
