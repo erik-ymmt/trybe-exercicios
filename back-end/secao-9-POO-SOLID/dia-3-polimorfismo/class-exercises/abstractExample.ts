f07a394421f89b6269c1e5255574b721fa1e0460
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-11 16:14:57 -0300

    add abstract exercise

--- back-end/secao-9-POO-SOLID/dia-3-polimorfismo/class-exercises/abstractExample.ts
+++ back-end/secao-9-POO-SOLID/dia-3-polimorfismo/class-exercises/abstractExample.ts
@@ -1,6 +1,6 @@
-class Animal {
+abstract class Animal {
   constructor(public name: string) { }
-  move() { console.log(`${this.name} está se movendo.`); }
+  abstract move(): void
 }
 class Bird extends Animal {
   move() { console.log(`${this.name} está voando.`); }
@@ -8,16 +8,16 @@ class Bird extends Animal {
 class Mammal extends Animal {
   move() { console.log(`${this.name} está andando.`); }
 }
+class Fish extends Animal {
+  move() { console.log(`${this.name} está nadando.`); }
+}
 
-const a = new Animal('Tubarão');
+const a = new Fish('Tubarão');
 const b = new Bird('Papagaio');
 const m = new Mammal('Tatu');
-
 const myMove = (animal: Animal) => {
   animal.move();
 }
 myMove(a);
 myMove(b);
 myMove(m);
-
-export {}
