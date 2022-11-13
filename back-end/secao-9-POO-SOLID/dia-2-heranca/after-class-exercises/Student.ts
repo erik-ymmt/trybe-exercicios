a0f226949ee4ba7f90c4a1a234b45fd422b77389
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-13 12:08:15 -0300

    add school exercise completed

--- back-end/secao-9-POO-SOLID/dia-2-heranca/after-class-exercises/Student.ts
+++ back-end/secao-9-POO-SOLID/dia-2-heranca/after-class-exercises/Student.ts
@@ -1,4 +1,5 @@
 import crypto from 'node:crypto';
+import EvaluationResult from './EvaluationResult';
 import IEnrollable from './IEnrollable';
 import Person from "./Person";
 
@@ -9,13 +10,10 @@ export default class Student extends Person implements IEnrollable {
   constructor (
     name: string,
     birthDate: Date,
-    private _examGrades: number[],
-    private _projectGrades: number[] 
+    public evaluationsResults: EvaluationResult[],
     ) {
     super(name, birthDate);
     this._id = this.generateEnrollment();
-    this.examGrades = _examGrades;
-    this.projectGrades = _projectGrades;
   }
 
   generateEnrollment(): string {
@@ -28,23 +26,14 @@ export default class Student extends Person implements IEnrollable {
     if (id.length < 16) throw Error('ID inválido.')
   }
 
-  verifyNumOfExamGrades(examGrades: number[]) {
-    if (examGrades.length > 4) throw Error('No máximo 4 notas de prova.')
-  }
-
-  verifyNumOfProjectGrades(grades: number[]) {
-    if (grades.length > 2) throw Error('No máximo 2 notas de trabalhos.')
-  }
-
   calculateGradesSum() {
-    const examGradesSum = this._examGrades.reduce((acc, cur) => acc + cur);
-    const projectGradesSum = this._projectGrades.reduce((acc, cur) => acc + cur);
-    return examGradesSum + projectGradesSum;
+    const gradesSum = this.evaluationsResults.reduce((acc, cur) => acc + cur.score, 0);
+    return gradesSum;
   }
 
   calculateGradesAvg() {
     const totalSum = this.calculateGradesSum();
-    const totalGrades = this._examGrades.length + this._projectGrades.length;
+    const totalGrades = this.evaluationsResults.length;
     return totalSum / totalGrades;
   }
 
@@ -52,26 +41,13 @@ export default class Student extends Person implements IEnrollable {
     return this._id;
   }
 
-  get examGrades() {
-    return this._examGrades;
-  }
-
-  get projectGrades() {
-    return this._projectGrades;
-  }
-
   set id(id: string) {
     this.verifyId(id);
     this._id = id;
   }
 
-  set examGrades(grades: number[]) {
-    this.verifyNumOfExamGrades(grades);
-    this._examGrades = grades;
-  }
-
-  set projectGrades(grades: number[]) {
-    this.verifyNumOfProjectGrades(grades);
-    this._projectGrades = grades;
-  }
+  // set evaluationsResults(grades: EvaluationResult[]) {
+  //   this.verifyNumOfExamGrades(grades);
+  //   this._evaluationsResults = grades;
+  // }
 }
