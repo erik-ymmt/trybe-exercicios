7c1277565d382bf6362c97a32998ce84526f582b
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-13 14:59:07 -0300

    add exercise 6

--- back-end/secao-9-POO-SOLID/dia-2-heranca/after-class-exercises/Restaurant.ts
+++ back-end/secao-9-POO-SOLID/dia-2-heranca/after-class-exercises/Restaurant.ts
@@ -3,6 +3,7 @@ import Exam from "./Exam";
 import Person from "./Person";
 import Student from "./Student";
 import Teacher from "./Teacher";
+import crypto from "node:crypto";
 
 class Item {
   constructor(private _name: string, private _price: number) {}
@@ -27,12 +28,14 @@ class Item {
 }
 
 class Order {
+  public _id: string;
   constructor(
     public _client: Person,
     private _items: Item[],
     private _paymentMethod: string,
     private _discount: number
   ) {
+    this._id = this.generateId();
     this._client = _client;
     this.items = _items;
     this.paymentMethod = _paymentMethod;
@@ -44,7 +47,7 @@ class Order {
   }
 
   set items(value: Item[]) {
-    if (value.length < 1) throw Error('Deve haver pelo menos um produto.');
+    if (value.length < 1) throw Error("Deve haver pelo menos um produto.");
     this._items = value;
   }
 
@@ -53,8 +56,9 @@ class Order {
   }
 
   set paymentMethod(value: string) {
-    const paymentMethods = ["dinheiro", "cartão", "vale"]
-    if (!paymentMethods.includes(value)) throw Error('Forma de pagamento deve ser dinheiro, cartão ou vale.');
+    const paymentMethods = ["dinheiro", "cartão", "vale"];
+    if (!paymentMethods.includes(value))
+      throw Error("Forma de pagamento deve ser dinheiro, cartão ou vale.");
     this._paymentMethod = value;
   }
 
@@ -63,8 +67,13 @@ class Order {
   }
 
   set discount(value: number) {
-    if (value < 0) throw Error('O desconto não pode ser negativo.');
+    if (value < 0) throw Error("O desconto não pode ser negativo.");
     this._discount = value;
+  }
+
+  generateId(): string {
+    const id = crypto.randomUUID();
+    return id;
   }
 
   calculateTotal() {
@@ -77,16 +86,60 @@ class Order {
   }
 }
 
+class OrderRepository {
+  private _orders: Order[] = [];
+
+  addOrder(order: Order) {
+    this._orders.push(order);
+  }
+
+  removeOrder(order: Order) {
+    const index = this._orders.indexOf(order);
+    if (index > -1) {
+      this._orders.splice(index, 1);
+    } else {
+      throw Error("Item não consta na lista.");
+    }
+  }
+
+  listByClient(client: string) {
+    const filter = this._orders.filter(
+      (order) => order._client.name === client
+    );
+    return filter;
+  }
+
+  listBySortedValue(value: "maior" | "menor") {
+    const math = value === "maior" ? -1 : 1;
+    const filter = this._orders.sort(
+      (a, b) =>
+        math * a.calculateTotalWithDiscount() + b.calculateTotalWithDiscount()
+    );
+    return filter;
+  }
+}
+
 const birth = new Date("1993-09-15");
-const teacher = new Teacher('tainha', birth, 2500, 'biologia');
+const teacher = new Teacher("tainha", birth, 2500, "biologia");
 const evaluation1 = new Exam(10, teacher);
 const grade1 = new EvaluationResult(evaluation1, 9);
 const grades = [grade1];
-const student = new Student('garcez', birth, grades)
+const student = new Student("garcez", birth, grades);
+const student2 = new Student("nicolau", birth, grades);
 const batata = new Item("batata", 5);
 const guarana = new Item("guarana", 3);
+const dolly = new Item("Dolly", 1.5);
 const order = new Order(student, [batata, guarana], "vale", 0.5);
+const order2 = new Order(student2, [dolly, guarana], "vale", 0.5);
+const orderList = new OrderRepository();
 
-console.log(order);
-console.log(order.calculateTotal());
-console.log(order.calculateTotalWithDiscount());
+orderList.addOrder(order);
+orderList.addOrder(order2);
+// orderList.removeOrder(order2);
+// console.log(orderList.listByClient("nicolau"));
+console.log(orderList.listBySortedValue("menor"));
+
+// console.log(order);
+// console.log(order.calculateTotal());
+// console.log(order.calculateTotalWithDiscount());
+// console.log(orderList);
