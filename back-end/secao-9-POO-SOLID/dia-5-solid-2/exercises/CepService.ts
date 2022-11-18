ae341227c0ad852bcfaabf809aca6d1680f77702
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-18 09:54:02 -0300

    add after class exercise 1

---
+++ back-end/secao-9-POO-SOLID/dia-5-solid-2/exercises/CepService.ts
@@ -0,0 +1,20 @@
+import ICepRepo from "./ICepAPI";
+
+
+class CepService {
+  private readonly cepApi: ICepRepo;
+
+  constructor(cepApi: ICepRepo) {
+    this.cepApi = cepApi;
+  }
+
+  addressByCep(cep: string, num: number) {
+    return this.cepApi.getAddressByCEP(cep, num);
+  }
+
+  cepByAddress(address: string, num: number) {
+    return this.cepApi.getCepByAddress(address, num);
+  }
+}
+
+export default CepService;
---
+++ back-end/secao-9-POO-SOLID/dia-5-solid-2/exercises/index.ts
@@ -0,0 +1,20 @@
+import CepService from './CepService';
+import FooCepAPI from './FooCepAPI';
+
+async function main() {
+  const cepAPI = new FooCepAPI();
+  const cepSvc = new CepService(cepAPI);
+
+  console.log(
+    'get address by cep', 
+    '->', 
+    await cepSvc.addressByCep('00.000.000', 10),
+  );
+  console.log(
+    'get cep by address', 
+    '->', 
+    await cepSvc.cepByAddress('street foo, between bar and baz', 10),
+  );
+}
+
+main();
