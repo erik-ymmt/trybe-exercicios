4a04d6a264d94a4abc410511d766891230509d57
Erik Yamamoto <erik.ymmt@gmail.com>
2023-02-15 15:56:42 -0300

    feat: exercise 2

--- ciencia_da_computacao/secao-1-python/dia-3-tests/exercise2.py
+++ ciencia_da_computacao/secao-1-python/dia-3-tests/exercise2.py
@@ -1,2 +1,26 @@
-def verify_num():
-    
+def verify_num(input):
+    decoded = ""
+    for letter in input:
+        if letter in ["A", "B", "C"]:
+            decoded += "2"
+        elif letter in ["D", "E", "F"]:
+            decoded += "3"
+        elif letter in ["G", "H", "I"]:
+            decoded += "4"
+        elif letter in ["J", "K", "L"]:
+            decoded += "5"
+        elif letter in ["M", "N", "O"]:
+            decoded += "6"
+        elif letter in ["P", "Q", "R", "S"]:
+            decoded += "7"
+        elif letter in ["T", "U", "V"]:
+            decoded += "8"
+        elif letter in ["W", "X", "Y"]:
+            decoded += "9"
+        else:
+            decoded += letter
+    print(decoded)
+
+
+verify_num("1-HOME-SWEET-HOME")
+verify_num("MY-MISERABLE-JOB")
